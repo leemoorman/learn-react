00338537731b674ec3a3fa3acc1c1593bdd9e3e8
leemoorman <Leethalweapon487@gmail.com>
2025-10-16 12:37:25 -0400

    changed text

--- src/index.js
+++ src/index.js
@@ -6,6 +6,6 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <h1>Hi Lee</h1>
-    <h2>Loving this</h2>
+    <h2>Hello World!</h2>
   </React.StrictMode>
 );
